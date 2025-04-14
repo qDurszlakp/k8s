c287b1d6eac6bad40533c7b6f9eb9675a0626d55
durszlak <pawel8marshall@gmail.com>
2025-04-15 00:47:24 +0200

    Tests refactor

--- App/src/test/java/com/sandbox/k8s/controller/AbstractDbIntegrationTest.java
+++ App/src/test/java/com/sandbox/k8s/controller/AbstractDbIntegrationTest.java
@@ -3,37 +3,27 @@ package com.sandbox.k8s.controller;
 import org.junit.ClassRule;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(initializers = AbstractDbIntegrationTest.Initializer.class)
 public abstract class AbstractDbIntegrationTest {
 
     @ClassRule
+    @SuppressWarnings("resource")
     public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.1")
             .withDatabaseName("testDb")
             .withUsername("testUser")
             .withPassword("testPass");
 
-    static {
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
         postgres.start();
-    }
-
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        @Override
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues values = TestPropertyValues.of(
-                    "spring.datasource.url=" + postgres.getJdbcUrl(),
-                    "spring.datasource.username=" + postgres.getUsername(),
-                    "spring.datasource.password=" + postgres.getPassword()
-            );
-            values.applyTo(configurableApplicationContext);
-        }
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 
 }
