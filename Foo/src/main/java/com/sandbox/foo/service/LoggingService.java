5710ef25aa1ca552753a83ed35f80d26a00faf7f
durszlak <pawel8marshall@gmail.com>
2025-04-16 21:15:16 +0200

    Added basic kafka producer

--- Foo/src/main/java/com/sandbox/foo/service/LoggingService.java
+++ Foo/src/main/java/com/sandbox/foo/service/LoggingService.java
@@ -1,6 +1,8 @@
 package com.sandbox.foo.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +10,23 @@ import java.util.UUID;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class LoggingService {
+
+    private static final String KAFKA_TOPIC = "TOPIC_1";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Scheduled(fixedRate = 10000)
     public void logMessage() {
-        log.info("[Foo] Log message: {}", UUID.randomUUID().toString());
+        String randomUUID = UUID.randomUUID().toString();
+        String message = "[Foo] Random log message: " + randomUUID;
+
+        try {
+            kafkaTemplate.send(KAFKA_TOPIC, message);
+            log.debug("Message sent to Kafka topic {}: {}", KAFKA_TOPIC, message);
+        } catch (Exception e) {
+            log.error("Error sending message to Kafka topic {}: {}", KAFKA_TOPIC, e.getMessage(), e);
+        }
     }
 } 
